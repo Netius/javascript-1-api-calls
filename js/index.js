9100bbcd9cddde4460aea7c530ee290528ef2c0b
Fabio Neto <fabiomoraisnetosp@hotmail.com>
2020-04-21 08:55:09 +0200

    Working API Service i Azure

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-const url = "https://elephant-api.herokuapp.com/elephants";
+// const url = "https://elephant-api.herokuapp.com/elephants";
+const url = "https://myapitestup.azurewebsites.net";
 const corsEnableUrl = "https://noroffcors.herokuapp.com/" + url;
 
 
@@ -23,19 +24,39 @@ function displayElephants(elephants) {
     const container = document.querySelector(".elephant-container");
     let html = "";
 
-    for (let i = 0; i < 4; i++) {
-        console.log(elephants[i].name);
 
-        if (!elephants[i].name) { //SKIP ONE i IN THE LOOP - NICE TO KNOW
-            continue;
-        }
-        html += `<div>
-        <img class="image" src="${elephants[i].image}">
-        <h3>${elephants[i].name}</h3>
-        <a href ="detail.html?name=${elephants[i].name}">Details</a>
-        </div>`;
 
-    }
+
+
+    html += `<div>
+         <img class="image" src="${elephants[0].background_image}">
+         <h2>${elephants[0].title}</h2>
+         <h3>${elephants[0].description}</h3>
+         <a href ="detail.html?name=${elephants[0].link}">Details</a>
+         </div>`;
+
+
+
+
+
+
+
+
+
+
+    // for (let i = 0; i < 4; i++) {
+    //     console.log(elephants[i].name);
+
+    //     if (!elephants[i].name) { //SKIP ONE i IN THE LOOP - NICE TO KNOW
+    //         continue;
+    //     }
+    //     html += `<div>
+    //     <img class="image" src="${elephants[i].image}">
+    //     <h3>${elephants[i].name}</h3>
+    //     <a href ="detail.html?name=${elephants[i].name}">Details</a>
+    //     </div>`;
+
+    // }
 
     container.innerHTML = html;
 }
